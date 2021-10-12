df2c74a0c43940ad06e77fc536f1686f317d0971
Ignacio Rodríguez Villasuso <ignaciorodvil@gmail.com>
2021-10-12 19:43:03 -0300

    New changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import HIW3 from './assets/HIW 3.png';
 import illusFot from './assets/ILLUS FOOTER.png';
 import logoFot from './assets/ISO LOGO.svg';
 import smiley from './assets/SMILEY.png';
+import estrela from './assets/estrela.png';
 import {useState} from "react"
 import axios from "axios";
 
@@ -26,7 +27,11 @@ function App() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
 
-
+  // console.log(window.addEventListener("load"))
+  window.addEventListener('load', (event) => {
+    setTimeout(function(){document.body.classList.remove('preload')}, 1000)
+  });
+  console.log(window)
   const handleClose = () => {
     setName("")
     setEmail("")
@@ -42,9 +47,13 @@ function App() {
   const handleShow = () => setShow(true);
   const handleSent = async () => {
     setLoading(true)
+    try{
     await axios.post("/users", {name:name , email:email})
     setSent(true)
     setLoading(false)
+    } catch {
+      setLoading(false)
+    }
   }
 
   const updateName = (e)=>{
@@ -62,7 +71,7 @@ function App() {
             <Modal.Header style={{backgroundColor: "#FEC031", borderWidth: 0, textAlign: "center"}} closeButton>
               <h4 style={{fontWeight: "700", fontSize: "28px", marginLeft:"auto"}}>Solicitação de cadastro</h4>
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body style= {{paddingBottom: "0px"}}>
               <p class="pModal">A Genti ainda está na versão beta e temos coisinhas para ajustar. Se você quiser fazer parte dessa jornada/experimento e ter um primeiro encontro, deixe seus dados e entraremos em contato.</p>
               <div style={{marginTop:"6%"}}>
                 <h4 style={{float:"left", fontSize: "16px", fontWeight: "700", marginRight: "15%", marginBottom: "8px"}}>Nome</h4><p class="pModal">O nome pelo qual iremos te chamar</p>
@@ -72,27 +81,33 @@ function App() {
                 <h4 style={{float:"left", fontSize: "16px", fontWeight: "700", marginRight: "15%", marginBottom: "8px"}}>Email</h4><p class="pModal">É assim que a Genti entra em contato com você</p>
                 <input onChange={updateEmail} style={{width: "100%", borderRadius:"5px", borderWidth: "1px", marginTop:"1%"}}></input>
               </div>
-              <button disabled={!name || !email || !validateEmail(email)} class="customButton" type="button" onClick={handleSent} style={{margin: "6% auto", display:"block"}}>
+              <div style={{margin: "6% auto", display:"block", textAlign:"center"}}>
+              <button disabled={!name || !email || !validateEmail(email)} class="customButton" type="button" onClick={handleSent}>
                 {loading ? 
                 (<Spinner animation="border" role="status">
                     <span className="visually-hidden">Loading...</span>
                   </Spinner>) 
                 : "Enviar solicitação"}
                 </button>
-              <p class="pModal" style={{textAlign: "center"}}>Tem dúvidas ou sugestões? Email pra Genti no <span style={{color:"#E5792A"}}>hello@genti.io</span> </p>
+              </div>
             </Modal.Body>
+            <Modal.Footer style={{backgroundColor: "#F9F8EC", borderWidth: 0, textAlign: "center"}}>
+                <p class="pModal" style={{textAlign: "center"}}>Tem dúvidas ou sugestões? Email pra Genti no <a class="pChica2" href="mailto:hello@genti.co">hello@genti.io</a></p>
+            </Modal.Footer>
           </>)
           : (<>
             <Modal.Header style={{backgroundColor: "#FEC031", borderWidth: 0, textAlign: "center"}} closeButton>
               <h4 style={{fontWeight: "700", fontSize: "28px", marginLeft:"auto"}}>Solicitação enviada</h4>
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body style= {{paddingBottom: "0px"}}>
             <img src={smiley} style={{width: "40%", margin: "6% auto", display:"block"}}></img>
             <h4 style={{fontWeight: "700", fontSize: "20px", marginLeft:"auto", textAlign:"center"}}>Agradecemos por fazer parte da Genti!</h4>
             <p class="pModal">Entraremos em contato em breve para te informar em detalhes como realizar sua primeira conexão.</p>
-            <h4 style={{fontWeight: "700", fontSize: "28px", marginLeft:"auto", marginTop:"5%", marginBottom:"5%", textAlign: "center"}} onClick={handleClose}>Voltar</h4>
-            <p class="pModal" style={{textAlign: "center"}}>Tem dúvidas ou sugestões? Email pra Genti no <span style={{color:"#E5792A"}}>hello@genti.io</span> </p>
+            <h4 id="voltar" onClick={handleClose}>Voltar</h4>
             </Modal.Body>
+            <Modal.Footer style={{backgroundColor: "#F9F8EC", borderWidth: 0, textAlign: "center"}}>
+                <p class="pModal" style={{textAlign: "center"}}>Tem dúvidas ou sugestões? Email pra Genti no <a class="pChica2" href="mailto:hello@genti.co">hello@genti.io</a></p>
+            </Modal.Footer>
           </>)
           }
       </Modal>
@@ -114,15 +129,22 @@ function App() {
           <h3 class="textLoop">o sócio</h3>
           </TextLoop>
           <h3 id="h32">que você busca</h3>
-          <button class="customButton" type="button" onClick={handleShow}>Testar a Genti</button>
+          <button class="customButton" type="button" onClick={handleShow} style={{marginTop: "10%"}}>Testar a Genti</button>
         </Col>
         <Col md={0} lg={7} className="d-none d-lg-block">
         <img src={cover} style={{width: "95%", display: "block", marginLeft: "auto", marginRight: "auto"}}></img>
         </Col>
       </Row>
+      <div style={{backgroundColor:"#F9F8EC", padding:"7% 0 10% 0"}}>
+        <div id="newDiv">
+          <img src={estrela} style={{width: "184px", marginBottom: "6%"}}></img>
+          <h3 className="h3new" style={{marginBottom:"32px"}}>Nossa IA te contecta com pessoas que te ajudam a alcançar seus objetivos.</h3>
+          <h3 className="h3new">Genti é sua nova aliada na hora de gerar essas conexões de impacto para sua carreira.</h3>
+        </div>
+      </div>
       <div style={{backgroundColor: "#FEC031", padding: "4% 0%"}}>
         <h2 id="h2mid" >Como funciona?</h2>
-      <Row id="mainRow" style={{padding:0, marginBottom: "5%"}}>
+      <Row id="mainRow" style={{padding:0, marginBottom: "5%", backgroundColor: "transparent"}}>
         <Col md={12} lg={7}>
         <img src={cover2} style={{width: "100%", display: "block"}}></img>
         </Col>
@@ -161,7 +183,9 @@ function App() {
         <div style={{margin:"0 auto 1% auto"}}>
         <img id="circle" src={circle}></img>
         <h2 id="h2final" >Sua comunidade está te esperando </h2>
-        <button class="customButton" type="button" style={{margin: "2.5% auto", display:"block"}} onClick={handleShow}>Testar a Genti</button>
+        <div style={{margin: "2.5% auto", display:"block", textAlign:"center", backgroundColor: "transparent"}}>
+        <button class="customButton" type="button" style={{marginTop: "0px"}} onClick={handleShow}>Testar a Genti</button>
+        </div>
         <h4 id="h4final" >O mundo que queremos construir está ao alcance de nossa vontade</h4>
         <img id="illusFot" src={illusFot} ></img>
         </div>
